9c6019fe039aa1aee7af3dde240b0e2858f1df81
newcomer <newcomer@liwenjiadeMacBook-Pro.local>
2015-06-25 15:48:33 +0800

    Create my own commonlib

---
+++ src/main/java/net/shiz/utils/FileUtils.java
@@ -0,0 +1,25 @@
+package net.shiz.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by liwenjia@youzan.com on 2015/06/25 .
+ */
+public class FileUtils {
+
+    /**
+     * Try to load file from several possible location:
+     * 1. classpath
+     * 2. relative path
+     * 3. absolute path
+     * 4. jar file
+     * 5. URL
+     * @param filelocation
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static FileInputStream load(String filelocation)throws FileNotFoundException{
+        return null;
+    }
+}
